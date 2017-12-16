0b18d8c09480fddca4ffef4872e3d62d74d3c39e
Tom <tom.jorissen.it@gmail.com>
2017-12-16 14:08:14 +0100

    Day 11.2

--- src/main/java/be/tomjo/advent/day11/HexGrid.java
+++ src/main/java/be/tomjo/advent/day11/HexGrid.java
@@ -1,9 +1,11 @@
 package be.tomjo.advent.day11;
 
 import be.tomjo.advent.Util;
-import com.google.common.primitives.Ints;
+
+import java.util.function.BinaryOperator;
 
 import static be.tomjo.advent.day11.Vector.IDENTITY;
+import static com.google.common.primitives.Ints.max;
 import static java.util.Arrays.stream;
 
 public class HexGrid {
@@ -12,11 +14,46 @@ public class HexGrid {
         String input = Util.readInput("11.txt").replace("\r\n", "");
 
         System.out.println("Solution 11.1: "+fewestStepsToEndDestination(input));
+        System.out.println("Solution 11.2: "+furthestStepsReached(input));
     }
 
     public static int fewestStepsToEndDestination(String inputDirections){
-        Vector direction = stream(inputDirections.split(",")).map(Vector::direction).reduce(IDENTITY, Vector::add);
-        return Ints.max(direction.getX(), direction.getY(), direction.getZ());
+        Vector reducedDirection = reduceDirections(inputDirections, Vector::add);
+        return max(reducedDirection.getX(), reducedDirection.getY(), reducedDirection.getZ());
+    }
+
+    public static int furthestStepsReached(String inputDirections){
+        IntHolder furthestStepCounter = new IntHolder(0);
+        BinaryOperator<Vector> reduceOperation = (v1, v2) -> {
+            Vector result = v1.add(v2);
+            int max = max(result.getX(), result.getY(), result.getZ());
+            if(max > furthestStepCounter.getValue()){
+                furthestStepCounter.setValue(max);
+            }
+            return result;
+        };
+        reduceDirections(inputDirections, reduceOperation);
+        return furthestStepCounter.getValue();
+    }
+
+    private static Vector reduceDirections(String inputDirections, BinaryOperator<Vector> reduceOperation){
+        return stream(inputDirections.split(",")).map(Vector::direction).reduce(IDENTITY, reduceOperation);
+    }
+
+    static class IntHolder {
+        private int value;
+
+        public IntHolder(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
     }
 
 }
---
+++ src/main/java/be/tomjo/advent/day12/Pipes.java
@@ -0,0 +1,4 @@
+package be.tomjo.advent.day12;
+
+public class Pipes {
+}
