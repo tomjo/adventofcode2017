bb7333653de1adaced924327ff1cb63ab484002b
Tom <tom.jorissen.it@gmail.com>
2017-12-19 00:51:42 +0100

    Day 17.2

--- src/main/java/be/tomjo/advent/day17/SpinLock.java
+++ src/main/java/be/tomjo/advent/day17/SpinLock.java
@@ -1,11 +1,22 @@
 package be.tomjo.advent.day17;
 
-import static be.tomjo.advent.Util.indexOf;
-
 public class SpinLock {
 
     public static void main(String[] args) {
         System.out.println("Solution 17.1: " + part1(371));
+        System.out.println("Solution 17.2: " + part2(371));
+    }
+
+    public static int part2(int input) {
+        int currentPosition = 0;
+        int valueAfterZero = 0;
+        for (int value = 1; value < 50000001; value++) {
+            currentPosition = (currentPosition + input) % value + 1;
+            if(currentPosition == 1){
+                valueAfterZero = value;
+            }
+        }
+        return valueAfterZero;
     }
 
     public static int part1(int input) {
@@ -17,11 +28,11 @@ public class SpinLock {
             size++;
             insert(buffer, size, currentPosition, value);
         }
-        return buffer[indexOf(buffer, 2017) + 1];
+        return buffer[currentPosition + 1];
     }
 
 
-    public static void insert(int[] buffer, int size, int position, int value) {
+    private static void insert(int[] buffer, int size, int position, int value) {
         System.arraycopy(buffer, position, buffer, position + 1, size - position - 1);
         buffer[position] = value;
     }
