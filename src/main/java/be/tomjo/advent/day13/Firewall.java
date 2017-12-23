18e95e6184604f23b06c38432afb9487c69f3106
Tom <tom.jorissen.it@gmail.com>
2017-12-23 15:46:35 +0100

    Optimized Day 13.2 even further: down to ~30ms by not checking all delays

--- src/main/java/be/tomjo/advent/day13/Firewall.java
+++ src/main/java/be/tomjo/advent/day13/Firewall.java
@@ -14,16 +14,16 @@ public class Firewall {
 
     public static void main(String[] args) {
         String input = readInput("13.txt");
-        System.out.println("Solution 13.1: "+benchmark(()->part1(input)));
-        System.out.println("Solution 13.2: "+benchmark(()->part2(input)));
+        System.out.println("Solution 13.1: " + benchmark(() -> part1(input)));
+        System.out.println("Solution 13.2: " + benchmark(() -> part2(input)));
     }
 
-    public static int part1(String input){
+    public static int part1(String input) {
         Map<Integer, Integer> firewall = createFirewall(input);
         return getSeverity(firewall);
     }
 
-    public static int part2(String input){
+    public static int part2(String input) {
         Map<Integer, Integer> firewall = createFirewall(input);
         return fewestStepsToPass(firewall);
     }
@@ -32,10 +32,12 @@ public class Firewall {
         return stream(input.split("\r\n")).map(s -> s.split(": ")).collect(toMap(p -> parseInt(p[0]), p -> parseInt(p[1])));
     }
 
-    public static int fewestStepsToPass(Map<Integer,Integer> firewall){
+    public static int fewestStepsToPass(Map<Integer, Integer> firewall) {
         int delay = 0;
-        while(!tryPassingWithDelay(firewall, delay)){
-            delay++;
+        int highestDepth = firewall.values().stream().mapToInt(i -> i).max().orElseThrow(IllegalStateException::new);
+        int skipSize = (highestDepth - 1) * 2;
+        while (!tryPassingWithDelay(firewall, delay)) {
+            delay += skipSize;
         }
         return delay;
     }
@@ -44,13 +46,13 @@ public class Firewall {
         boolean passes = true;
         for (Map.Entry<Integer, Integer> e : firewall.entrySet()) {
             passes = !getsCaughtInLayer(e.getKey() + delay, e.getValue());
-            if(!passes)
+            if (!passes)
                 break;
         }
         return passes;
     }
 
-    public static int getSeverity(Map<Integer,Integer> firewall){
+    public static int getSeverity(Map<Integer, Integer> firewall) {
         return firewall.entrySet().stream()
                 .filter(e -> getsCaughtInLayer(e.getKey(), e.getValue()))
                 .mapToInt(e -> e.getKey() * e.getValue())
@@ -62,6 +64,6 @@ public class Firewall {
         //layer1    =   caught at   1, 3, 5
         //layer4    =   caught at   4, 10, 16
         //layern    =   caught at   n, n+(depth-1)*2, n+(depth-1)*2+(depth-1)*2
-        return layer % ((depth-1)*2) == 0;
+        return layer % ((depth - 1) * 2) == 0;
     }
 }
